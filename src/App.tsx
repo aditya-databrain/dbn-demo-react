6250f60bf1d89da52a71f1e931ca1cdd4b18e63f
Aditya Yaduvanshi <aditya@usedatabrain.com>
2023-06-09 12:43:26 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "dbn-ui-test-1/web";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <dbn-dashboard
+        token="f2834487-d196-463c-9a57-9e02f78c7dda"
+        dashboardId="dev-orders"
+      />
+    </div>
+  );
+}
+
+export default App;
