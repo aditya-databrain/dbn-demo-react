83cccefb11fde110df846fcda13ae27e38654d94
Aditya Yaduvanshi <aditya@usedatabrain.com>
2023-06-09 12:46:40 +0530

    fix ts build error

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
+      {/* @ts-ignore */}
       <dbn-dashboard
         token="f2834487-d196-463c-9a57-9e02f78c7dda"
         dashboardId="dev-orders"
